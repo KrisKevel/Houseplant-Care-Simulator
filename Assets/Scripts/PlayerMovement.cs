8d704d8fe28bdf34a7fa915eec53384d95927823
KrisKevel <kevel.kristina@gmail.com>
2020-11-07 21:01:45 +0200

    Room barebone model, cube moving around

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.z -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+        rb.MovePosition(pos);
+    }
+}
