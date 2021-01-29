5187f91d2e2d6cb6ee6f2194aa235fe2aa8c57e6
KrisKevel <kevel.kristina@gmail.com>
2021-01-29 21:30:09 +0200

    Grid system

---
+++ Assets/Scripts/Placement/Grid.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField]
+    private float _size = 1f;
+
+    public Vector3 GetNearestPointOnGrid(Vector3 position)
+    {
+        position -= transform.position;
+
+        int xCount = Mathf.RoundToInt(position.x / _size);
+        int yCount = Mathf.RoundToInt(position.y / _size);
+        int zCount = Mathf.RoundToInt(position.z / _size);
+
+        Vector3 result = new Vector3(
+            (float)xCount * _size,
+            (float)yCount * _size,
+            (float)zCount * _size);
+
+        result += transform.position;
+
+        return result;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        for (float x = 0; x < 40; x += _size)
+        {
+            for (float z = 0; z < 40; z += _size)
+            {
+                var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
+                Gizmos.DrawSphere(point, 0.1f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Placement/PlantPlacer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantPlacer : MonoBehaviour
+{
+    public Grid FloorGrid;
+    public GameObject PlantPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+
+                PlacePlant(hitInfo.point);
+            }
+        }
+    }
+
+    void PlacePlant(Vector3 nearPoint)
+    {
+        var finalPosition = FloorGrid.GetNearestPointOnGrid(nearPoint);
+        Instantiate<GameObject>(PlantPrefab).transform.position = finalPosition;
+    }
+}
