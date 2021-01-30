ccd442733c84676fa57027b09f1833705e38c043
KrisKevel <kevel.kristina@gmail.com>
2021-01-30 15:14:54 +0200

    Picking up plants

---
+++ Assets/Scripts/Placement/PickUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private Transform theDest;
+
+    private void Awake()
+    {
+        theDest = GameObject.Find("Destination").transform;
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        gameObject.transform.position = theDest.position;
+        gameObject.transform.parent = theDest;
+    }
+
+    void OnMouseUp()
+    {
+        gameObject.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+}
