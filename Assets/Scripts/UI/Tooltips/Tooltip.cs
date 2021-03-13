58d5b52046df1bc5a5294b366627af60a245d7c0
KrisKevel <kevel.kristina@gmail.com>
2021-03-13 17:19:28 +0200

    Added basic tooltip system

---
+++ Assets/Scripts/UI/Tooltips/Tooltip.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[ExecuteInEditMode()]
+public class Tooltip : MonoBehaviour
+{
+    public TextMeshProUGUI HeaderField;
+    public TextMeshProUGUI ContentField;
+    public LayoutElement LayoutElement;
+
+    public int CharacterWrapLimit;
+
+    private RectTransform _rectTransform;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        gameObject.SetActive(false);
+    }
+
+    public void SetText(string content, string header = "")
+    {
+        if (string.IsNullOrEmpty(header))
+        {
+            HeaderField.gameObject.SetActive(false);
+        } 
+        else
+        {
+            HeaderField.gameObject.SetActive(true);
+            HeaderField.text = header;
+
+        }
+
+        ContentField.text = content;
+        
+        int headerLenght = HeaderField.text.Length;
+        int contentLenght = ContentField.text.Length;
+
+        LayoutElement.enabled = (headerLenght > CharacterWrapLimit || contentLenght > CharacterWrapLimit) ? true : false;
+    }
+
+    private void Update()
+    {
+        if (Application.isEditor)
+        {
+            int headerLenght = HeaderField.text.Length;
+            int contentLenght = ContentField.text.Length;
+
+            LayoutElement.enabled = (headerLenght > CharacterWrapLimit || contentLenght > CharacterWrapLimit) ? true : false;
+        }
+
+        Vector2 position = Input.mousePosition;
+
+        float pivotX = position.x / Screen.width;
+        float pivotY = position.y / Screen.height;
+
+
+        _rectTransform.pivot = new Vector2(pivotX, pivotY);
+        transform.position = position;
+    }
+}
