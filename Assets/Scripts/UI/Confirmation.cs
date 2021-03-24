d1c21769970ce864a6a0efda6f7e60ed9cabe2d0
KrisKevel <kevel.kristina@gmail.com>
2021-03-24 19:34:12 +0200

    Order confirmation

---
+++ Assets/Scripts/UI/Confirmation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Confirmation : MonoBehaviour
+{
+    public TextMeshProUGUI Days;
+    public TextMeshProUGUI PlantName;
+
+    private void Awake()
+    {
+        Events.OnBuyPlant += Open;
+        gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        Events.OnBuyPlant -= Open;
+    }
+
+    public void Open(GameObject houseplant)
+    {
+        HouseplantData houseplentData = houseplant.gameObject.GetComponent<HouseplantHealth>().Houseplant;
+        Days.text = houseplentData.DaysForDelivery.ToString();
+        PlantName.text = houseplentData.HouseplantName;
+        gameObject.SetActive(true);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
