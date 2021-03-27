aa90d14858b01f27d843ca693822890c9aae4b4d
KrisKevel <kevel.kristina@gmail.com>
2021-03-27 21:45:45 +0200

    Color changes and clicking outside monitor closing it

--- Assets/Scripts/Computer/Monitor.cs
+++ Assets/Scripts/Computer/Monitor.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Monitor : MonoBehaviour
+public class Monitor : MonoBehaviour, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    private bool _mouseIsOver = false;
+
     void Awake()
     {
         Events.OnUseComputer += OpenPanel;
@@ -24,5 +26,32 @@ public class Monitor : MonoBehaviour
     public void CloseMonitor()
     {
         gameObject.SetActive(false);
+    }
+
+    //https://answers.unity.com/questions/947856/how-to-detect-click-outside-ui-panel.html?page=1&pageSize=5&sort=votes 
+    //Ziplock9000's answer + BluishGreenPro's comment
+    public void OnDeselect(BaseEventData eventData)
+    {
+        if (!_mouseIsOver)
+            gameObject.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _mouseIsOver = true;
+        EventSystem.current.SetSelectedGameObject(gameObject);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _mouseIsOver = false;
+        EventSystem.current.SetSelectedGameObject(gameObject);
+    }
+    //https://answers.unity.com/questions/947856/how-to-detect-click-outside-ui-panel.html?page=1&pageSize=5&sort=votes 
+    //Ziplock9000's answer + BluishGreenPro's comment
+
+    private void Update()
+    {
+        //print(_mouseIsOver);
     }
 }
