ad264470844663b9f3090683164923f86b10f5fe
KrisKevel <kevel.kristina@gmail.com>
2021-04-09 20:25:06 +0300

    Added separate hints

--- Assets/Scripts/Computer/PlantInfoPage.cs
+++ Assets/Scripts/Computer/PlantInfoPage.cs
@@ -22,10 +22,10 @@ public class PlantInfoPage : MonoBehaviour
         TextMeshProUGUI[] texts = gameObject.GetComponentsInChildren<TextMeshProUGUI>();
         _name = texts[0];
         _generalInfo = texts[2];
-        _wateringName = texts[3];
-        _wateringInfo = texts[4];
-        _lightName = texts[5];
-        _lightinfo = texts[6];
+        _wateringName = texts[4];
+        _wateringInfo = texts[5];
+        _lightName = texts[7];
+        _lightinfo = texts[8];
         gameObject.SetActive(false);
     }
 
@@ -66,14 +66,17 @@ public class PlantInfoPage : MonoBehaviour
     {
         if (moisture < 33.3)
         {
+            _wateringName.text = "[ Dry ]";
             _wateringName.color = Color.red;
         }
         else if (moisture < 66.6)
         {
+            _wateringName.text = "[ Moist ]";
             _wateringName.color = Color.green;
         }
         else
         {
+            _wateringName.text = "[ Wet ]";
             _wateringName.color = Color.blue;
         }
     }
@@ -82,14 +85,17 @@ public class PlantInfoPage : MonoBehaviour
     {
         if (light < 33.3)
         {
+            _lightName.text = "[ Shadow ]";
             _lightName.color = Color.grey;
         }
         else if (light < 66.6)
         {
+            _lightName.text = "[ Light ]";
             _lightName.color = new Color(0.9f, 0.82f, 0.6f, 1);
         }
         else
         {
+            _lightName.text = "[ Sunny ]";
             _lightName.color = Color.yellow;
         }
     }
